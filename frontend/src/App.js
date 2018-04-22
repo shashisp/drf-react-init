1627d553bc5ccfbb7578bb0ed97f7c25780338a9
Shashi Kumar P <shashiessp@gmail.com>
2018-04-22 00:46:39 -0500

    Revert "init redux'"
    This reverts commit 2238b075c0e9f7f4189a3b66a0ce66161f759327.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import {Route, Switch, BrowserRouter} from 'react-router-dom';
-
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import ponyApp from "./reducers";
-
-import PonyNote from "./components/PonyNote";
-import NotFound from "./components/NotFound";
-
-let store = createStore(ponyApp);
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={PonyNote} />
-            <Route component={NotFound} />
-          </Switch>
-        </BrowserRouter>
-      </Provider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to lol</h1>
+        </header>
+        <p className="App-intro">
+            A react app with django as a backend.
+        </p>
+      </div>
     );
   }
 }
--- frontend/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import notes from "./notes";
-
-
-const ponyApp = combineReducers({
-  notes,
-})
-
-export default ponyApp;
